10e27f6574bbe11e005b07e484071ce4a537e90a
DIOR <diertojiev@yandex.ru>
2024-12-09 10:34:24 +0300

    Вариант кода д/з от учителя на урок 150

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,76 +1,86 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
-import "./charList.scss";
-// import abyss from "../../resources/img/abyss.jpg";
-import MarvelService from "../../services/MarvelService";
-import { Component } from "react";
-import ErrorMessage from "../errorMessage/ErrorMessage";
-import Spinner from "../spinner/Spinner";
+import {Component} from 'react';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import MarvelService from '../../services/MarvelService';
+import './charList.scss';
 
 class CharList extends Component {
-	state = {
-		characters: [],
-		loading: true,
-		error: false,
-	};
 
-	marvelService = new MarvelService();
+    state = {
+        charList: [],
+        loading: true,
+        error: false
+    }
+    
+    marvelService = new MarvelService();
 
-	componentDidMount() {
-		this.marvelService
-			.getAllCharacters()
-			.then(this.onCharsLoaded)
-			.catch(this.onError);
-	}
+    componentDidMount() {
+        this.marvelService.getAllCharacters()
+            .then(this.onCharListLoaded)
+            .catch(this.onError)
+    }
 
-	onCharsLoaded = (characters) => {
-		this.setState({ loading: false, characters });
-	};
+    onCharListLoaded = (charList) => {
+        this.setState({
+            charList,
+            loading: false
+        })
+    }
 
-	onError = () => {
-		this.setState({ loading: false, error: true });
-	};
+    onError = () => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
 
-	render() {
-		const { characters, loading, error } = this.state;
+    // Этот метод создан для оптимизации, 
+    // чтобы не помещать такую конструкцию в метод render
+    renderItems(arr) {
+        const items =  arr.map((item) => {
+            let imgStyle = {'objectFit' : 'cover'};
+            if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+                imgStyle = {'objectFit' : 'unset'};
+            }
+            
+            return (
+                <li 
+                    className="char__item"
+                    key={item.id}>
+                        <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+                        <div className="char__name">{item.name}</div>
+                </li>
+            )
+        });
+        // А эта конструкция вынесена для центровки спиннера/ошибки
+        return (
+            <ul className="char__grid">
+                {items}
+            </ul>
+        )
+    }
 
-		const arrChars = characters;
+    render() {
 
-		const errorMessage = error ? <ErrorMessage /> : null;
-		const spinner = loading ? <Spinner /> : null;
+        const {charList, loading, error} = this.state;
+        
+        const items = this.renderItems(charList);
 
-		return (
-			<div className='char__list'>
-				{errorMessage}
-				{spinner}
-				<ul className='char__grid'>
-					{arrChars && !errorMessage && !spinner
-						? arrChars.map((item) => (
-								<Character
-									key={item.id}
-									name={item.name}
-									img={item.thumbnail}
-								/>
-						  ))
-						: null}
-				</ul>
-				<button className='button button__main button__long'>
-					<div className='inner'>load more</div>
-				</button>
-			</div>
-		);
-	}
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? items : null;
+
+        return (
+            <div className="char__list">
+                {errorMessage}
+                {spinner}
+                {content}
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
 }
-
-const Character = ({ name, img }) => {
-	return (
-		<li className='char__item'>
-			<img
-				src={img}
-				alt='image character'
-			/>
-			<div className='char__name'>{name}</div>
-		</li>
-	);
-};
 
 export default CharList;
